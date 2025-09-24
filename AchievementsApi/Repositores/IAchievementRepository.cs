e14c377ad198e67b886792e891539200d5217ca4
lasm16 <lasm16@yandex.ru>
2025-09-24 22:14:54 +0300

    Добавил начальную логику работы сервиса

---
+++ AchievementsApi/Repositores/IAchievementRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Models;
+
+namespace AchievementsApi.Repositores
+{
+    public interface IAchievementRepository
+    {
+        Task<Achievement> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<List<Achievement>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task AddAsync(Achievement achievement, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Achievement achievement, CancellationToken cancellationToken = default);
+        Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+    }
+}
