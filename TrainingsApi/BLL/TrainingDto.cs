2702c8478870fa46616264f0a78d9db5c0905bf9
Xam1l1on <kir.solomatin@gmail.com>
2025-10-04 18:21:37 +0300

    Добавил паттерн State (состояние). Пришлось поменять сущность и дто.

--- TrainingsApi/BLL/TrainingDto.cs
+++ TrainingsApi/BLL/TrainingDto.cs
@@ -7,6 +7,6 @@
         public string? Description { get; set; }
         public DateTime Date { get; set; }
         public double DurationInMinutes { get; set; }
-        public bool IsCompleted { get; set; }
+        public string? Status { get; set; }
     }
 }
