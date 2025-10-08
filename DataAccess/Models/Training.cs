000a0b4c3014486252ca6a51d126a81d398d80bc
Xam1l1on <kir.solomatin@gmail.com>
2025-10-08 20:48:50 +0300

    Изменение имен классов

--- DataAccess/Models/Training.cs
+++ DataAccess/Models/Training.cs
@@ -7,7 +7,7 @@
         public string? Description { get; set; }
         public DateTime Date { get; set; }
         public double DurationInMinutes { get; set; }
-        public string Status { get; set; }
+        public string? Status { get; set; }
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
         public bool IsDeleted { get; set; }
