0dcc3bc243a7b0110967d156d0e9ff4b75f2606f
Виталя <razor52@mail.ru>
2025-10-20 22:11:19 +0300

    Update UserApi/UserRequest

--- UsersApi/BLL/DTOs/UserRequest.cs
+++ UsersApi/BLL/DTOs/UserRequest.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace UsersApi.BLL.DTOs
 {
     public class UserRequest
-    {        
+    {
+        [Range(1, int.MaxValue)]
         public int Id { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(140, MinimumLength = 3)]
         public string Name { get; set; } = string.Empty;
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(140, MinimumLength = 3)]
         public string Surname { get; set; } = string.Empty;
+        [Required(AllowEmptyStrings = false)]
+        [EmailAddress]
+        [StringLength(256)]
         public string Email { get; set; } = string.Empty;
     }
 }
