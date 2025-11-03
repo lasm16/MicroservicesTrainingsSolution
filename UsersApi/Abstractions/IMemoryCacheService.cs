9e07107409522bd106c0680f9da7eba688ce5268
lasm16 <lasm16@yandex.ru>
2025-11-03 17:52:34 +0300

    добавил сервис кэширования

---
+++ UsersApi/Abstractions/IMemoryCacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace UsersApi.Abstractions
+{
+    public interface IMemoryCacheService
+    {
+        bool TryGet<T>(object key, out T? value);
+        void Set<T>(object key, T value);
+    }
+}
---
+++ UsersApi/BLL/Services/MemoryCacheService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using UsersApi.Abstractions;
+using UsersApi.Properties;
+
+namespace UsersApi.BLL.Services
+{
+    public class MemoryCacheService(IMemoryCache cache,
+        IOptions<AppSettingsConfig> settingsConfig) : IMemoryCacheService
+    {
+        private readonly AppSettingsConfig _settingsConfig = settingsConfig.Value;
+        public bool TryGet<T>(object key, out T? value)
+        {
+            return cache.TryGetValue(key, out value);
+        }
+
+        public void Set<T>(object key, T value)
+        {
+            var options = GetMemoryCacheOptions();
+            cache.Set(key, value, options);
+        }
+
+        private MemoryCacheEntryOptions GetMemoryCacheOptions()
+        {
+            var expirationTime = TimeSpan.FromSeconds(_settingsConfig.CacheSettings!.AbsoluteExpirationFromSeconds);
+            return new MemoryCacheEntryOptions().SetAbsoluteExpiration(expirationTime);
+        }
+    }
+}
